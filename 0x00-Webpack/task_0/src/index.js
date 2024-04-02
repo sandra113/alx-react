dc24c3a3a730b79525dd01d57d4360d7546dd640
sandra113 <sandramabonga303@gmail.com>
2024-04-02 15:05:22 +0000

    react

---
+++ 0x00-Webpack/task_0/src/index.js
@@ -0,0 +1,8 @@
+import $ from 'jquery';
+
+
+$(document).ready(function() {
+	$('body').append('<P>Paragraph 1</p>');
+	$('body').append('<P>Paragraph 1</p>');
+	$('body').append('<P>Paragraph 1</p>');
+});
