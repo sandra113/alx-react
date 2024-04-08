27a925797ace7d0a9ce08d8fc9917fc3ed7438b0
sandra113 <sandramabonga303@gmail.com>
2024-04-08 18:20:42 +0000

    Embedding expressions, functions

---
+++ 0x01-react_intro/task_1/dashboard/src/App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import './App.css';
+
+
+function App() {
+	return (
+		<div className="App">
+		<header className="App-header">
+		{/* Holberton logo */}
+		<img src="holberton_logo.png" alt="Holberton Logo" />
+		{/* Heading */}
+		<h1>School dashboard</h1>
+		</header>
+
+		<div className="App-body">
+		{/* Body content */}
+		<p>Login to access the full dashboard</p>
+		</div>
+		<footer classsName="App-footer">
+		{/* Footer content*/}
+		<p>Copyright 2020 - Holberton School</p>
+		</footer>
+		</div>
+	);
+}
